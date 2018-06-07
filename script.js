06a75a6cec5b46e86101da520dcbca6252f8685e
Gianpaulo M. Soares <gianpaulo.soares@totvs.com.br>
2018-06-07 13:24:10 -0300

    version 1.0

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-[].forEach.call(document.querySelectorAll('[data-qa]'), e => e.style = "border: red 1px solid; before: 'test' ")
+(() => {
+	const id = 'qa-inspector';
+	const head = document.getElementsByTagName('head')[0];
+	const body = document.getElementsByTagName('body')[0];
+
+	const style = document.createElement('style');
+	style.innerText = `
+		[data-qa] {
+			border:red 1px solid !important;
+			position:relative !important;
+		}
+		
+		#${id} {
+			position: absolute;
+			bottom: 0;
+			right: 0;
+			padding: 5px 10px;
+			background-color: red;
+		}`;
+
+	const insp = document.createElement('div');
+	insp.id = id;
+
+	head.appendChild(style);
+	body.appendChild(insp);
+
+	[].forEach.call(document.querySelectorAll('[data-qa]'), el => {
+		el.addEventListener('mouseenter', ev => {
+			document.getElementById(id).innerText = ev.target.getAttribute('data-qa')
+        })
+    });
+})()
